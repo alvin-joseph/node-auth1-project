33de88c7aefbf8ea9e81b0e6582005587d6dc8a5
alvin-joseph <aznman1234@gmail.com>
2021-06-14 19:43:13 -0500

    finished mvp

--- api/server.js
+++ api/server.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const helmet = require("helmet");
 const cors = require("cors");
+const session = require('express-session');
+const Store = require('connect-session-knex')(session);
+
+const authRouter = require('./auth/auth-router');
+const usersRouter = require('./users/users-router');
 
 /**
   Do what needs to be done to support sessions with the `express-session` package!
@@ -17,9 +22,32 @@ const cors = require("cors");
 
 const server = express();
 
+server.use(session({
+  name: 'chocolatechip',
+  secret: 'keep it secret', // .env file
+  cookie: {
+    maxAge: 1000 * 60 * 10,
+    secure: false, // if true, only works over TLS/https
+    httpOnly: true, // if true, cookie not in document
+    //sameSite: 'none' //enable 3rd party cookies over https
+  },
+  resave: false, // required by some session stores
+  saveUninitialized: false, // session not saved automatically
+  store: new Store({
+    knex: require('../data/db-config'),
+    tableName: 'sessions',
+    sidfieldname: 'sid',
+    createtable: true,
+    clearInterval: 1000 * 60 * 10
+  })
+}));
+
 server.use(helmet());
 server.use(express.json());
 server.use(cors());
+
+server.use('/api/users', usersRouter);
+server.use('/api/auth', authRouter);
 
 server.get("/", (req, res) => {
   res.json({ api: "up" });
